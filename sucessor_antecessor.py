81593723519888b840bea31f2c717089b59dabf5
Ramon Henrique Pereira Neves <89115263+RamonHPN@users.noreply.github.com>
2022-05-02 21:35:37 -0300

    Add files via upload

---
+++ sucessor_antecessor.py
@@ -0,0 +1,11 @@
+n = 0 
+antecessor = 0
+sucessor = 0
+
+n = int(input("Digite um numero: "))
+antecessor = n - 1
+sucessor = n + 1
+  
+print("antecessor: ", antecessor)
+print("numero: ", n)
+print("sucessor: ", sucessor)
